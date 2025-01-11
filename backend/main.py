640c71a11f8225157f40bbae8d3b3601773c42f3
daniel-halpern <dhalpern4040@gmail.com>
2025-01-11 17:14:04 -0500

    Added class_schedule and os support

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,9 @@
 from helper_functions import *
 
 # Constants
-example_ics_name = "./backend/data/josh-calendar-export.ics"
-example_csv_name = "./backend/data/student_schedules.csv"
+base_dir = os.path.dirname(os.path.abspath(__file__))
+example_ics_name = os.path.join(base_dir, "data/josh-calendar-export.ics")
+example_csv_name = os.path.join(base_dir, "data/student_schedules.csv")
 
 if __name__ == "__main__":
     # Test creating an csv file
